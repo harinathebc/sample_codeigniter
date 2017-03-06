8a88c168d942059c210c14fd7eedc5f53f4c9e82
Harinath <harinath.tavva@ebcdata.com>
2017-03-06 16:05:06 +0530

    PDF PATH
    PDF PATH

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1769,7 +1769,7 @@ class User extends CI_Controller {
 		//$stylesheet6 = file_get_contents('http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic');
 		//echo $stylesheet;exit;
 		//echo APPPATH . 'vendor/autoload.php';
-		echo require FCPATH.'vendor/autoload.php';
+		///echo require FCPATH.'vendor/autoload.php';
 		require FCPATH.'vendor/autoload.php';
 		//echo BASEPATH . 'vendor/autoload.php';exit;
 		$pdf = new mPDF();
